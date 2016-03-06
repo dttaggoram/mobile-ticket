a1327b792d114aefac1b6cc13de975bb9a6a9279
David Tagg-Oram <david@tagg-oramproductions.co.uk>
2016-03-06 17:51:52 +0000

    updated

--- auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-$salt = "H7J2D9872rbfo1gfkjbi27bgo92hgnwoueb93yfky2p";
-$hash = "298fbqiuobf(8£hgo";
-$resethash = "9283471927598";
-$passwordhash = 'jkejbf92h112bdb13fh20f';
-$saltphone = "kub398fn20ebfo123ubsoi3bf";
-$worldpayresponse = "heiakb93hi3hd1902hf";
-?>
